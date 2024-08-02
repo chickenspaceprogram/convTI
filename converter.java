b511e182aa6c5b15d461dcb80c6f35a12af0cf30
Athena Boose <pestpestthechicken@yahoo.com>
2024-08-01 23:53:24 -0700

    Created Header class.
    Signed-off-by: Athena Boose <pestpestthechicken@yahoo.com>

---
+++ converter.java
@@ -0,0 +1,7 @@
+public class Converter {
+    public static void main(String[] args) {
+        Header head = new Header();
+        byte[] header_array = head.make_array(69);
+    }
+}
+// apologies if this code is actually horrible or something, I'm still in the process of learning Java and OOP in general.
