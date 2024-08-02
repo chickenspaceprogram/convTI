58dac2c1aeed4dac97921567c37a215d8a7e3c2e
Athena Boose <pestpestthechicken@yahoo.com>
2024-08-02 11:54:07 -0700

    Added Word class to easily represent the MSB and LSB of a long.

--- converter.java
+++ converter.java
@@ -1,7 +1,7 @@
 public class Converter {
     public static void main(String[] args) {
-        Header head = new Header();
-        byte[] header_array = head.make_array(69);
+        Header header = new Header();
+        System.out.println(header);
     }
 }
 // apologies if this code is actually horrible or something, I'm still in the process of learning Java and OOP in general.
