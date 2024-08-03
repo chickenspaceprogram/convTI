923e6541aa7274668b9c8d4e74f0c481d821606b
Athena Boose <pestpestthechicken@yahoo.com>
2024-08-02 20:14:56 -0700

    Worked a bunch on the input from command line, it still kinda sucks.

--- Word.java
+++ Word.java
@@ -2,7 +2,7 @@ public class Word {
     public byte MSB;
     public byte LSB;
 
-    public void makeWord(short value) {
+    public Word(short value) {
         this.LSB = (byte) (value & 0x00FF); // LSB
         this.MSB = (byte) ((value & 0xFF00) >> 8); // MSB
     }
